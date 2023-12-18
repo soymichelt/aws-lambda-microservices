18942f33a030a54fc370570b458246e1b07e6bd0
SoymichelDev <mtraatabladaa94@gmail.com>
2023-12-18 17:21:00 -0600

    refactor: update baseController to core service

--- core/src/shared/infrastructure/controllers/baseController.ts
+++ core/src/shared/infrastructure/controllers/baseController.ts
@@ -1,20 +1,23 @@
 import { Context } from 'aws-lambda';
+import { container } from 'tsyringe';
 import { ManagerRequestParsersController } from '@shared/infrastructure/controllers/managerRequestParsersController';
-import { inject } from 'tsyringe';
 import { DomainException } from '@shared/domain/exceptions/baseException';
 import { BaseResponseType } from '@shared/infrastructure/controllers/responses/baseResponseType';
+import { Logger } from '@shared/domain/loggers/logger';
 
 export abstract class BaseController<RequestType, ResponseType> {
+  protected logger: Logger;
   private managerRequestParser: ManagerRequestParsersController;
 
-  constructor(@inject('ManageRequestParsersController') managerRequestParser: ManagerRequestParsersController) {
-    this.managerRequestParser = managerRequestParser;
+  constructor() {
+    this.logger = container.resolve<Logger>('Logger');
+    this.managerRequestParser = container.resolve<ManagerRequestParsersController>('ManagerRequestParsersController');
   }
 
   public async execute(event: any, context: Context): Promise<BaseResponseType> {
     try {
       const request = this.parseEventToRequest(event, context);
-      const result = await this.run(request, context);
+      const result = await this.run(request, context, event);
 
       return this.generateSuccessResult(result);
     }
@@ -23,7 +26,7 @@ export abstract class BaseController<RequestType, ResponseType> {
     }
   }
 
-  public abstract run(request: RequestType, context: Context): Promise<ResponseType>;
+  public abstract run(request: RequestType, context: Context, event: any): Promise<ResponseType>;
 
   protected getSuccessStatusCode(response: ResponseType): number {
     if (!response) {
@@ -49,6 +52,12 @@ export abstract class BaseController<RequestType, ResponseType> {
   }
 
   private generateErrorResult(error: DomainException): BaseResponseType {
+    this.logger.error({
+      ...(error.toPrimitives ? error.toPrimitives() : error),
+      stack: error.stack,
+      message: error.message,
+    });
+
     return {
       statusCode: error.status || 500,
       body: JSON.stringify({
